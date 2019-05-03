3dc07d544fb50188178a8e77e370dde55016f99d
CaffeineAndSquats <47569781+CaffeineAndSquats@users.noreply.github.com>
2019-05-03 12:33:53 +0200

    Update to mC part, needs to be tested

--- Lab07/mC/lab07/lab07.ino
+++ Lab07/mC/lab07/lab07.ino
@@ -8,7 +8,6 @@ IRM Lab07 : Closed-loop control I
 #include <Adafruit_MotorShield.h>
 #include <Wire.h>
 
-
 // global variables to control each motor
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_StepperMotor *myMotor1 = AFMS.getStepper(200, 1);
@@ -66,9 +65,7 @@ int move_steps (int steps, int dir, int motor) {
   // in the loop, move the desired motor in small steps (stepsize). Execute the loop until you moved
   // the whole distance
   step_count = 0;
-  switch_check = -1;
-  ref = digitalRead(ls);
-    
+      
   if(step_limit <= steps) {
     step_stop = step_limit;
   }
@@ -76,79 +73,52 @@ int move_steps (int steps, int dir, int motor) {
     step_stop = steps;
   }
 
-  /*
-  if (steps != 0 & out == 0) {
-    while (step_count <= step_stop) {
-      if (ls == 7) {
-        myMotor1.step(1, FORWARD, MICROSTEP);
-      }
-      else if (ls == 6) {
-        myMotor1.step(1, BACKWARD,MICROSTEP);
-      }
-      else if (ls == 5) {
-        myMotor2.step(1, FORWARD,MICROSTEP);
-      }
-      else if (ls == 4) {
-        myMotor2.step(1, BACKWARD,MICROSTEP);
-      }
-          
-      switch_press = digitalRead(ls);
-      if (switch_press != ref) {
-        switch_check = 0;
-        break;
-      } 
-      step_count++
+  if ((steps > 0) && (out == 0)) {
+    switch_check = -1;
+    if (digitalRead(ls)==LOW){
+      switch_check = 0;
     }
-  }
-  */
-
-  if (steps > 0 & out == 0) {
     if (ls == 7) {
-      while (step_count <= step_stop) {
-        myMotor1->step(1, FORWARD, MICROSTEP);
-        switch_press = digitalRead(ls);
-        if (switch_press != ref) {
+      while (step_count <= step_stop && switch_check == -1) {
+        myMotor1->step(stepSize, FORWARD, SINGLE);
+        if (digitalRead(ls) == LOW) {
           switch_check = 0;
           break;
         }
-        step_count++ ; 
+        step_count += step_size;
       }
     }
     else if (ls == 6) {
-      while (step_count <= step_stop) {
-        myMotor1->step(1, BACKWARD,MICROSTEP);
-        switch_press = digitalRead(ls);
-        if (switch_press != ref) {
+      while (step_count <= step_stop && switch_check == -1) {
+        myMotor1->step(stepSize, BACKWARD,SINGLE);
+        if (digitalRead(ls) == LOW) {
           switch_check = 0;
           break;
         }
-        step_count++; 
+        step_count += step_size;
       }
     }
     else if (ls == 5) {
-      while (step_count <= step_stop) {
-        myMotor2->step(1, FORWARD,MICROSTEP);
-        switch_press = digitalRead(ls);
-        if (switch_press != ref) {
+      while ((step_count <= step_stop) && (switch_check == -1)) {
+        myMotor2->step(stepSize, FORWARD,SINGLE);
+        if (digitalRead(ls) == LOW) {
           switch_check = 0;
           break;
         }
-        step_count++; 
+        step_count += step_size;
       }
     }
     else if (ls == 4) {
-      while (step_count <= step_stop) {
-        myMotor2->step(1, BACKWARD,MICROSTEP);
-        switch_press = digitalRead(ls);
-        if (switch_press != ref) {
+      while ((step_count <= step_stop) && (switch_check == -1)) {
+        myMotor2->step(stepSize, BACKWARD,SINGLE);
+        if (digitalRead(ls) == LOW) {
           switch_check = 0;
           break;
         }
-        step_count++; 
+        step_count += step_size; 
       }
     }
   }
-  
   
   // After running the loop, return ASCII for the switches
     if (switch_check == 0)  {
@@ -169,7 +139,7 @@ int move_steps (int steps, int dir, int motor) {
         // If switch 3 is pressed, return '3'
           out = 51;
           break;
-        default :
+        default :    // WHY FOR?
           out = 53;
           break;
       }
@@ -203,7 +173,6 @@ void setup() {
   pinMode(switch4, INPUT);
 
 }
-
 
 
 void loop() {
@@ -243,40 +212,55 @@ void loop() {
     // Check the first byte and if it is not '1' or '2' discard it
     // First byte determines the stage (stepper motor) that needs to be moved
     motor = (command[0]-'0');
-    if (motor != 1 | motor != 2) {
+    /*Serial.print("motor = ");
+    Serial.print(motor);
+    Serial.print('\n');*/    // for debugging purpose
+    if ((motor != 1) && (motor != 2)) {
       check = '5';
+      /*Serial.print("problem with motor");
+      Serial.print('\n');*/    // for debugging purpose
     }
   
     // Check the second byte and if it is not '1' or '2' discard it
     // Second byte determines the direction
     dir = (command[1]-'0');
-    if (dir != 1 | dir != 2) {
+    /*Serial.print("dir = ");
+    Serial.print(dir);
+    Serial.print('\n');*/    // for debugging purpose
+    if ((dir != 1) && (dir != 2)) {
       check = '5';
+      /*Serial.print("problem with dir");
+      Serial.print('\n');*/    // for debugging purpose
     }
       
     // Check that third to fifth bytes are between '0' and '9'
     // make sure to convert from chars to integers (subtract 48, the ASCII constant) and multiply accordingly
-    c4 = (command[4]-'0');
-    c3 = (command[3]-'0');
-    c2 = (command[2]-'0');
-    if (c4 >= 0 & c4 <= 9 & c3 >= 0 & c3 <= 9 & c2 >= 0 & c2 <= 9) {
+    c4 = (command[4]-'0');  // Serial.print("c4 = ");  Serial.print(c4);  Serial.print('\n');
+    c3 = (command[3]-'0');  // Serial.print("c3 = ");  Serial.print(c3);  Serial.print('\n');
+    c2 = (command[2]-'0');  // Serial.print("c2 = ");  Serial.print(c2);  Serial.print('\n');    // for debugging purpose
+    if ((c4 >= 0) && (c4 <= 9) && (c3 >= 0) && (c3 <= 9) && (c2 >= 0) && (c2 <= 9)) {
       steps = c4+10*c3+100*c2;
+      /*Serial.print(steps);
+      Serial.print('\n');*/    // for debugging purpose
     }
     else {
       check = '5';
+      /*Serial.print("problem with steps");
+      Serial.print('\n');*/    // for debugging purpose
     }
-    //steps = 1*(command[4]-'0')+10*(command[3]-'0')+100*(command[2]-'0');
     
     // If everything is fine, move the motors
     if (check == '5') {
       // if check == '5' something went wrong
       // do nothing, it will print out '5'
+      continue;
     }
     else {
       int_check = move_steps (steps, dir, motor);
-      check = int_check;    // divided into two steps to make sure the conversion is made correctly   
+      check = int_check;    // divided into two steps to make sure the conversion is made correctly  
+      /*Serial.print("it runs");
+      Serial.print('\n');*/    // for debugging purpose
     }
-    
     // Check is sent over the serial back to microprocessor:
     //     '0' if motor moved 
     //     '1' if switch 1 is pressed
@@ -285,6 +269,7 @@ void loop() {
     //     '4' if switch 4 is pressed
     //     '5' if command is bad
     Serial.print(check);
+    Serial.print('\n');
   
     // Reset the command
     command[0] = '\0';
